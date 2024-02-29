3d14f07265de24aa3ac76703c8a97566d4dc7a32
Frederik A. Evensen <fredcraft0@gmail.com>
2024-02-29 22:01:31 +0100

    Added making a triangle (i hope)

--- 3DCompulsory2/src/Mesh/Mesh.cpp
+++ 3DCompulsory2/src/Mesh/Mesh.cpp
@@ -1,1 +1,13 @@
 #include "Mesh.h"
+
+void Mesh::triangle(float size)
+{
+	a = size;
+	Vertex v0{-a, a, a, 1.0f, 0.f, 0.f};
+	Vertex v1{-a, -a, a, 1.0f, 0.f, 0.f};
+	Vertex v2{a, -a, a, 1.0f, 0.f, 0.f};
+
+	mIndices.emplace_back(0);
+	mIndices.emplace_back(1);
+	mIndices.emplace_back(2);
+}
--- 3DCompulsory2/src/Mesh/Mesh.h
+++ 3DCompulsory2/src/Mesh/Mesh.h
@@ -6,13 +6,22 @@
 
 struct Vertex
 {
-	float x, y, z;
+	float x, y , z;
+	float v0, v1, v2;
+	Vertex(float x, float y, float z, float r, float g, float b) : x(x), y(y), z(z), v0(r), v1(g), v2(b)
+	{
+	}
 };
 
 class Mesh
 {
+	float a{1.0f};
+
 public:
 	std::vector<Vertex> mVertices;
 	std::vector<GLuint> mIndices;
+
+	void triangle(float size);
+
 };
 
