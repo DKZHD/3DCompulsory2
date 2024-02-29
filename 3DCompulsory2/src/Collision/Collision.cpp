af8943ebc4a76586b8619a8b7c55a8f370aa00f1
Jonathannygaard <jonathan.nygaard1310@gmail.com>
2024-03-01 00:26:07 +0100

    Collision functions

--- 3DCompulsory2/src/Collision/Collision.cpp
+++ 3DCompulsory2/src/Collision/Collision.cpp
@@ -1,12 +1,31 @@
 #include "Collision.h"
+#include <iostream>
 #include "../Mesh/Mesh.h"
+
+Collision::Collision(glm::vec3 position, glm::vec3 scale, Cube* realCube)
+{
+    min = position - scale;
+    max = position + scale;
+
+    cube = realCube;
+}
+
 
 void Collision::checkWorldCollision()
 {
     
 }
 
-void Collision::checkCollision(Cube& cube1, Cube& cube2)
+void Collision::checkCollision(Collision& cube1)
 {
-    if(cube1.)
+    if(cube1.min.x < max.x && cube1.max.x > min.x &&
+        cube1.min.y < max.y && cube1.max.y > min.y &&
+        cube1.min.z < max.z && cube1.max.z > min.z)
+    {
+        std::cout << "Collision" << std::endl;
+        if(cube.bIsPlayer == true || cube1.cube.bIsPlayer == true)
+        {
+            std::cout << "Player Collision" << std::endl;
+        }
+    }
 }
--- 3DCompulsory2/src/Collision/Collision.h
+++ 3DCompulsory2/src/Collision/Collision.h
@@ -1,10 +1,18 @@
 #pragma once
+#include <glm/vec3.hpp>
+#includ
 
 class Cube;
 
 class Collision
 {
 public:
+    glm::vec3 min;
+    glm::vec3 max;
+
+    Cube* cube;
+    
+    Collision(glm::vec3 position, glm::vec3 scale, Cube* realCube);
     void checkWorldCollision();
-    void checkCollision(Cube& cube1, Cube& cube2);
+    void checkCollision(Collision& cube1);
 };
