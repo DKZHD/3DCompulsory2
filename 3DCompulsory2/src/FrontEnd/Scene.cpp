d64ec303837558c60dd38c0a6ed0a5b3e9adb382
DKZHD <dkvarberg@gmail.com>
2024-03-04 09:40:52 +0100

    Changed Scene

--- 3DCompulsory2/src/FrontEnd/Scene.cpp
+++ 3DCompulsory2/src/FrontEnd/Scene.cpp
@@ -1,13 +1,19 @@
 ﻿#include "Scene.h"
 
-void Scene::Init()
+#include "../Camera/Camera.h"
+#include "../Mesh/Mesh.h"
+
+void Scene::Init(glm::vec3 init_cam_pos)
 {
+	camera->cameraPos = init_cam_pos;
 }
 
 void Scene::Load()
 {
+
 }
 
 void Scene::Destroy()
 {
+
 }
--- 3DCompulsory2/src/FrontEnd/Scene.h
+++ 3DCompulsory2/src/FrontEnd/Scene.h
@@ -1,9 +1,18 @@
 ﻿#pragma once
+#include "glm/vec3.hpp"
+
+
+class Camera;
+class Mesh;
 
 class Scene
 {
+	Mesh* mesh;
+	Camera* camera;
+	int LastIndex = 0;
 public:
-	void Init();
+	Scene(Mesh* mesh, Camera* camera) : mesh(mesh), camera(camera){}
+	void Init(glm::vec3 init_cam_pos);
 	void Load();
 	void Destroy();
 };
